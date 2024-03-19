b08be2471afbd38a0786693c09549bc03755564a
Khairi Rusydi <krusydi@palo-it.com>
2024-03-19 14:50:11 +0800

    feat: persist cache using redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { HttpModule } from '@nestjs/axios';
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 import TrafficController from './traffic/traffic.controller';
 import TrafficService from './traffic/traffic.service';
 
 @Module({
-  imports: [HttpModule, CacheModule.register({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot(),
+    HttpModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
+  ],
   controllers: [TrafficController],
   providers: [TrafficService],
 })
